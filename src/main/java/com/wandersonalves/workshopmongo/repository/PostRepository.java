b3747e5cb3089caa50bfb445e28d174b429c594b
Wanderson <emailwandersonalves@gmail.com>
2023-01-13 11:45:41 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/wandersonalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/wandersonalves/workshopmongo/repository/PostRepository.java
@@ -3,8 +3,11 @@ package com.wandersonalves.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wandersonalves.workshopmongo.domain.User;
+import com.wandersonalves.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+	
+
 }
